77d89006d5b66095aba33d905333de924e8c6caf
unknown <mandaniainaprecieux@yahoo.com>
2026-05-16 23:42:36 +0300

    Creation Question model

---
+++ db/migrate/20260516203720_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[8.1]
+  def change
+    create_table :questions do |t|
+      t.text :content
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
