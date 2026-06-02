fcd8ce9e1f352e9bb74ab69271b47fa4c7eaef26
unknown <mandaniainaprecieux@yahoo.com>
2026-06-02 21:19:52 +0300

    ray

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module PersonalityTest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # Please, add to the `ignore` list any other `lib` subdirectories 
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
